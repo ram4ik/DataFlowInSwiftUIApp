8e5e6a7aef7c67ff1313f8450683735889c1542e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-13 20:37:45 +0300

    Initial Commit

---
+++ DataFlowInSwiftUIApp/DataFlowInSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowInSwiftUIAppApp.swift
+//  DataFlowInSwiftUIApp
+//
+//  Created by ramil on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowInSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
